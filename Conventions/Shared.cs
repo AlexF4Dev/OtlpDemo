5420d8b9bbd2a175fd47b8e814198aedbdfa81ae
Eric Erhardt <eric.erhardt@microsoft.com>
2023-08-23 09:32:28 -0600

    Fix view name to work with 8.0-rc1 (#1)

--- Conventions/Shared.cs
+++ Conventions/Shared.cs
@@ -12,12 +12,22 @@ public static class Extensions
 {
     public static void AddServiceDefaults(this IHostApplicationBuilder builder)
     {
-        var resourceBuilder = ResourceBuilder.CreateDefault().AddService(builder.Environment.ApplicationName);
+        builder.AddServiceDiscovery();
 
+        builder.AddOpenTelemetry();
+    }
+
+    private static void AddServiceDiscovery(this IHostApplicationBuilder builder)
+    {
         builder.Services.AddServiceDiscovery().AddConfigurationServiceEndPointResolver();
 
         HttpClientFactoryServiceCollectionExtensions.ConfigureHttpClientDefaults(builder.Services,
             b => b.AddServiceDiscovery().AddStandardResilienceHandler());
+    }
+
+    private static void AddOpenTelemetry(this IHostApplicationBuilder builder)
+    {
+        var resourceBuilder = ResourceBuilder.CreateDefault().AddService(builder.Environment.ApplicationName);
 
         builder.Logging.AddOpenTelemetry(builder =>
         {
@@ -41,7 +51,7 @@ public static class Extensions
                     builder.AddOtlpExporter();
 
                     builder.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel", "System.Net.Http");
-                    builder.AddView("request-duration",
+                    builder.AddView("http.server.request.duration",
                         new ExplicitBucketHistogramConfiguration
                         {
                             Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]
